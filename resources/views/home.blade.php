8a88de91d480225be6f870a43396bae61fdcd425
lazzerex <nahmsix2005@gmail.com>
2025-04-07 17:39:55 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
                     <div class="sm:text-center lg:text-left">
                         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl animate-fade-in">
                             <span class="block">Hi, I'm</span>
-                            <span class="block text-primary animate-slide-in">Nathan</span>
+                            <span class="block text-primary animate-slide-in">Your Name</span>
                         </h1>
                         <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 animate-fade-in-up" style="animation-delay: 200ms">
                             I'm a passionate full-stack developer specializing in Rust and web technologies. 
